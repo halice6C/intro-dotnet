f1ad4acf7a346c17ab043eb41cdb05683865c8da
halice <115983607+halice6C@users.noreply.github.com>
2024-04-18 11:47:32 +0200

    feat: display persons and details

--- src/WebApplication2/Controllers/HomeController.cs
+++ src/WebApplication2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataProvider;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,23 @@ namespace WebApplication2.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IPersonRepository _personRepository;
+        public HomeController(ILogger<HomeController> logger, IPersonRepository personRepository)
         {
             _logger = logger;
+            _personRepository = personRepository;
         }
-
+        [Route("personDetails/{id}")]
+         public IActionResult PersonDetails(Guid id)
+    {
+         var person = _personRepository.Get(id);
+         ViewBag.Id = id;
+            return View(person);
+    }
         public IActionResult Index()
         {
-            return View();
+            var people = _personRepository.GetAll();
+            return View(people);
         }
 
         public IActionResult Privacy()
--- src/WebApplication2/Startup.cs
+++ src/WebApplication2/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataProvider;
+using Microsoft.AspNetCore.DataProtection;
+
 
 namespace WebApplication2
 {
@@ -22,6 +26,7 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IPersonRepository, PersonRepository>();
             services.AddControllersWithViews();
         }
 
@@ -47,6 +52,8 @@ namespace WebApplication2
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+           
+                
             });
         }
     }
